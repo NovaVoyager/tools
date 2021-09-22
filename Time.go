8ca385cf1005a4da96109d1171ecb80103c78315
蒲强 <pu.qiang@qq.com>
2021-09-22 18:20:08 +0800

    ``

--- Time.go
+++ Time.go
@@ -1,1 +1,14 @@
 package tools
+
+import "time"
+
+const (
+	TimeNormalFormat = "2006-01-02 15:04:05"
+)
+
+var (
+	cstZone = time.FixedZone("CST", 3600*8)
+	TimeStr = func() string {
+		return time.Now().In(cstZone).Format(TimeNormalFormat)
+	}
+)
