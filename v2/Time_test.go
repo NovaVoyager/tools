96dbccd3e4ea47b28cd613e2faf9b8c5126576b8
puqiang <puqiang@comisys.net>
2022-04-28 10:35:20 +0800

    rm

--- v2/Time_test.go
+++
@@ -1,8 +0,0 @@
-package tools
-
-import "testing"
-
-func TestTimeStr(t *testing.T) {
-	t.Log(TimeDateFormat, "-->", TimeStr(TimeDateFormat, cstZone))
-	t.Log(DateFormat, "-->", TimeStr(DateFormat, cstZone))
-}
--- v2/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/miaogu-go/tools/v2
-
-go 1.16
-
-require github.com/shopspring/decimal v1.3.1
