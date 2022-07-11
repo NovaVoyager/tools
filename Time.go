83724866206be2b37e2c4c21165c2f42cce3bf8a
蒲强 <pu.qiang@qq.com>
2022-07-11 22:59:52 +0800

    秒转日期

--- Time.go
+++ Time.go
@@ -1,6 +1,8 @@
 package tools
 
-import "time"
+import (
+	"time"
+)
 
 const (
 	//TimeDateFormat 时间日期格式
@@ -19,3 +21,26 @@ var (
 		return time.Now().In(cst).Format(format)
 	}
 )
+
+//SecondToDayHMS 秒转换为天 时 分 秒
+func SecondToDayHMS(second int64) (int64, int64, int64, int64) {
+	if second == 0 {
+		return 0, 0, 0, 0
+	}
+
+	oneHour := 60 * 60
+	oneMinute := 60
+	oneDay := oneHour * 24
+
+	// 天    总秒数/一天的秒数=几天
+	d := second / int64(oneDay)
+	// 小时  不够一天的秒数可以换算成几小时
+	h := second / int64(oneHour)
+	//h1 := second % int64(oneDay) / int64(oneHour)
+	// 分钟  不够一小时的秒数可以换算成几分钟
+	m := second % int64(oneHour) / int64(oneMinute)
+	//秒数	不够一分钟的秒数可以换算成几秒
+	s := second % int64(oneHour) % int64(oneMinute) % int64(oneMinute)
+
+	return d, h, m, s
+}
--- Time_test.go
+++ Time_test.go
@@ -6,3 +6,75 @@ func TestTimeStr(t *testing.T) {
 	t.Log(TimeDateFormat, "-->", TimeStr(TimeDateFormat, cstZone))
 	t.Log(DateFormat, "-->", TimeStr(DateFormat, cstZone))
 }
+
+func TestSecondToDayHMS(t *testing.T) {
+	type args struct {
+		second int64
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int64
+		want1 int64
+		want2 int64
+		want3 int64
+	}{
+		{
+			name:  "1",
+			args:  args{second: 86400},
+			want:  1,
+			want1: 24,
+			want2: 0,
+			want3: 0,
+		},
+		{
+			name:  "2",
+			args:  args{second: 90301},
+			want:  1,
+			want1: 25,
+			want2: 5,
+			want3: 1,
+		},
+		{
+			name:  "3",
+			args:  args{second: 3600},
+			want:  0,
+			want1: 1,
+			want2: 0,
+			want3: 0,
+		},
+		{
+			name:  "4",
+			args:  args{second: 10},
+			want:  0,
+			want1: 0,
+			want2: 0,
+			want3: 10,
+		},
+		{
+			name:  "4",
+			args:  args{second: 5*86400 + 7200 + 300 + 1},
+			want:  5,
+			want1: 122,
+			want2: 5,
+			want3: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, got2, got3 := SecondToDayHMS(tt.args.second)
+			if got != tt.want {
+				t.Errorf("SecondToDayHMS() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("SecondToDayHMS() got1 = %v, want %v", got1, tt.want1)
+			}
+			if got2 != tt.want2 {
+				t.Errorf("SecondToDayHMS() got2 = %v, want %v", got2, tt.want2)
+			}
+			if got3 != tt.want3 {
+				t.Errorf("SecondToDayHMS() got3 = %v, want %v", got3, tt.want3)
+			}
+		})
+	}
+}
